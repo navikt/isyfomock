078e694c80d02b5e17d32492160c0af0e7c9ba32
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2022-04-04 12:34:30 +0200

    Opprett dialogmelding fellesformat xml (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,17 @@ group = "no.nav.syfo"
 version = "0.0.1"
 
 object Versions {
+    const val jaxb = "2.3.1"
     const val ktor = "1.6.8"
     const val kluent = "1.68"
     const val mockk = "1.12.3"
     const val jackson = "2.13.1"
+    const val javaTimeAdapter = "1.1.3"
     const val logback = "1.2.11"
     const val logstashEncoder = "7.0.1"
     const val micrometerRegistry = "1.8.4"
     const val spek = "2.0.18"
+    const val syfotjenester = "1.2021.06.09-13.09-b3d30de9996e"
     const val swaggerUi = "4.9.1"
 }
 
@@ -24,6 +27,13 @@ val githubUser: String by project
 val githubPassword: String by project
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/syfotjenester")
+        credentials {
+            username = githubUser
+            password = githubPassword
+        }
+    }
 }
 
 dependencies {
@@ -45,6 +55,14 @@ dependencies {
 
     // (De-)serialization
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Versions.jackson}")
+    implementation("javax.xml.bind:jaxb-api:${Versions.jaxb}")
+    implementation("org.glassfish.jaxb:jaxb-runtime:${Versions.jaxb}")
+    implementation("com.migesok:jaxb-java-time-adapters:${Versions.javaTimeAdapter}")
+
+    implementation("no.nav.syfotjenester:fellesformat:${Versions.syfotjenester}")
+    implementation("no.nav.syfotjenester:kith-base64:${Versions.syfotjenester}")
+    implementation("no.nav.syfotjenester:kith-dialogmelding:${Versions.syfotjenester}")
+    implementation("no.nav.syfotjenester:kith-hodemelding:${Versions.syfotjenester}")
 
     swaggerUI("org.webjars:swagger-ui:${Versions.swaggerUi}")
 
