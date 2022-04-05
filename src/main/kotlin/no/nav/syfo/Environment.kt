6a75abab47efbc79c8139a7f73b91ba25041c706
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2022-04-05 09:54:59 +0200

    Setup MQ send to padm2 (#4)

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -2,6 +2,28 @@ package no.nav.syfo
 
 data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
+    val mq: EnvironmentMQ = EnvironmentMQ(
+        mqQueueManager = getEnvVar("MQGATEWAY_NAME"),
+        mqHostname = getEnvVar("MQGATEWAY_HOSTNAME"),
+        mqApplicationName = "isyfomock",
+        mqPort = getEnvVar("MQGATEWAY_PORT", "1413").toInt(),
+        mqChannelName = getEnvVar("MQGATEWAY_CHANNEL_NAME"),
+        padm2Queuename = getEnvVar("PADM2_QUEUENAME"),
+    ),
+)
+
+data class EnvironmentMQ(
+    val mqQueueManager: String,
+    val mqHostname: String,
+    val mqApplicationName: String,
+    val mqPort: Int,
+    val mqChannelName: String,
+    val padm2Queuename: String,
+)
+
+data class ServiceUser(
+    val username: String = getEnvVar("SERVICEUSER_USERNAME"),
+    val password: String = getEnvVar("SERVICEUSER_PASSWORD"),
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
