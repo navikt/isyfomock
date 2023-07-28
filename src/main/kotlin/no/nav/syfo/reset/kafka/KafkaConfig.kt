942962a39f45a3286dcdfc0509f7974fad6b7f16
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-07-28 15:48:47 +0200

    String serializer

--- src/main/kotlin/no/nav/syfo/reset/kafka/KafkaConfig.kt
+++ src/main/kotlin/no/nav/syfo/reset/kafka/KafkaConfig.kt
@@ -3,6 +3,7 @@ package no.nav.syfo.reset.kafka
 import no.nav.syfo.kafka.KafkaEnvironment
 import no.nav.syfo.kafka.commonKafkaAivenProducerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
 import java.util.*
 
 fun kafkaTestdataResetHendelseProducerConfig(
@@ -10,6 +11,6 @@ fun kafkaTestdataResetHendelseProducerConfig(
 ): Properties {
     return Properties().apply {
         putAll(commonKafkaAivenProducerConfig(kafkaEnvironment))
-        this[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = String::class.java.canonicalName
+        this[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.canonicalName
     }
 }
