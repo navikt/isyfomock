c1a8e08c4e56724c2c4c424a48eaaacddb1aaf3a
Anders Rognstad <anders.rognstad@nav.no>
2024-04-24 15:07:34 +0200

    Flatten versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,22 @@
 group = "no.nav.syfo"
 version = "0.0.1"
 
-object Versions {
-    const val jaxb = "2.3.1"
-    const val kithApprecVersion = "2019.07.30-04-23-2a0d1388209441ec05d2e92a821eed4f796a3ae2"
-    const val ktor = "2.3.8"
-    const val kluent = "1.73"
-    const val mockk = "1.13.9"
-    const val jacksonDataType = "2.16.1"
-    const val javaTimeAdapter = "1.1.3"
-    const val json = "20231013"
-    const val logback = "1.4.14"
-    const val logstashEncoder = "7.4"
-    const val micrometerRegistry = "1.12.2"
-    const val mq = "9.3.3.0"
-    const val spek = "2.0.19"
-    const val syfotjenester = "1.2021.06.09-13.09-b3d30de9996e"
-    const val swaggerUi = "5.1.3"
-    const val kafka = "3.7.0"
-}
+val jaxbVersion = "2.3.1"
+val kithApprecVersion = "2019.07.30-04-23-2a0d1388209441ec05d2e92a821eed4f796a3ae2"
+val ktorVersion = "2.3.8"
+val kluentVersion = "1.73"
+val mockkVersion = "1.13.9"
+val jacksonDataTypeVersion = "2.16.1"
+val javaTimeAdapterVersion = "1.1.3"
+val jsonVersion = "20231013"
+val logbackVersion = "1.4.14"
+val logstashEncoderVersion = "7.4"
+val micrometerRegistryVersion = "1.12.2"
+val mqVersion = "9.3.3.0"
+val spekVersion = "2.0.19"
+val syfotjenesterVersion = "1.2021.06.09-13.09-b3d30de9996e"
+val swaggerUiVersion = "5.1.3"
+val kafkaVersion = "3.7.0"
 
 plugins {
     kotlin("jvm") version "1.9.23"
@@ -44,53 +42,53 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
-    implementation("io.ktor:ktor-serialization-jackson:${Versions.ktor}")
-    implementation("io.ktor:ktor-client-apache:${Versions.ktor}")
-    implementation("io.ktor:ktor-client-content-negotiation:${Versions.ktor}")
-    implementation("io.ktor:ktor-client-cio:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-content-negotiation:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-status-pages:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-netty:${Versions.ktor}")
+    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
+    implementation("io.ktor:ktor-client-apache:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
 
     // Logging
-    implementation("ch.qos.logback:logback-classic:${Versions.logback}")
-    implementation("net.logstash.logback:logstash-logback-encoder:${Versions.logstashEncoder}")
-    implementation("org.json:json:${Versions.json}")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+    implementation("org.json:json:$jsonVersion")
 
     // Metrics and Prometheus
-    implementation("io.ktor:ktor-server-metrics-micrometer:${Versions.ktor}")
-    implementation("io.micrometer:micrometer-registry-prometheus:${Versions.micrometerRegistry}")
+    implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:$micrometerRegistryVersion")
 
     // (De-)serialization
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Versions.jacksonDataType}")
-    implementation("javax.xml.bind:jaxb-api:${Versions.jaxb}")
-    implementation("org.glassfish.jaxb:jaxb-runtime:${Versions.jaxb}")
-    implementation("com.migesok:jaxb-java-time-adapters:${Versions.javaTimeAdapter}")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonDataTypeVersion")
+    implementation("javax.xml.bind:jaxb-api:$jaxbVersion")
+    implementation("org.glassfish.jaxb:jaxb-runtime:$jaxbVersion")
+    implementation("com.migesok:jaxb-java-time-adapters:$javaTimeAdapterVersion")
 
     // MQ
-    implementation("com.ibm.mq:com.ibm.mq.allclient:${Versions.mq}")
+    implementation("com.ibm.mq:com.ibm.mq.allclient:$mqVersion")
 
-    implementation("no.nav.syfotjenester:fellesformat:${Versions.syfotjenester}")
-    implementation("no.nav.syfotjenester:kith-base64:${Versions.syfotjenester}")
-    implementation("no.nav.syfotjenester:kith-dialogmelding:${Versions.syfotjenester}")
-    implementation("no.nav.syfotjenester:kith-hodemelding:${Versions.syfotjenester}")
-    implementation("no.nav.helse.xml:kith-apprec:${Versions.kithApprecVersion}")
+    implementation("no.nav.syfotjenester:fellesformat:$syfotjenesterVersion")
+    implementation("no.nav.syfotjenester:kith-base64:$syfotjenesterVersion")
+    implementation("no.nav.syfotjenester:kith-dialogmelding:$syfotjenesterVersion")
+    implementation("no.nav.syfotjenester:kith-hodemelding:$syfotjenesterVersion")
+    implementation("no.nav.helse.xml:kith-apprec:$kithApprecVersion")
 
     // Kafka
     val excludeLog4j = fun ExternalModuleDependency.() {
         exclude(group = "log4j")
     }
-    implementation("org.apache.kafka:kafka_2.13:${Versions.kafka}", excludeLog4j)
+    implementation("org.apache.kafka:kafka_2.13:$kafkaVersion", excludeLog4j)
 
-    swaggerUI("org.webjars:swagger-ui:${Versions.swaggerUi}")
+    swaggerUI("org.webjars:swagger-ui:$swaggerUiVersion")
 
-    testImplementation("io.ktor:ktor-server-test-host:${Versions.ktor}")
-    testImplementation("io.mockk:mockk:${Versions.mockk}")
-    testImplementation("org.amshove.kluent:kluent:${Versions.kluent}")
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:${Versions.spek}") {
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("org.amshove.kluent:kluent:$kluentVersion")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion") {
         exclude(group = "org.jetbrains.kotlin")
     }
-    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:${Versions.spek}") {
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion") {
         exclude(group = "org.jetbrains.kotlin")
     }
 }
